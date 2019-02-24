0fc4d3d3bbc332efdb979929b398f6a4b4501121
Vladimir <vladimir.losk90@gmail.com>
2019-02-24 04:28:38 -0500

    Update gallery & form submission handler

--- assets/vendor/form-submission-handler.js
+++ assets/vendor/form-submission-handler.js
@@ -1,9 +1,9 @@
-
-function validEmail(email) { // see:
+(function() {
+  function validEmail(email) {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     return re.test(email);
   }
-  
+
   function validateHuman(honeypot) {
     if (honeypot) {  //if hidden form filled up
       console.log("Robot Detected!");
@@ -12,13 +12,12 @@ function validEmail(email) { // see:
       console.log("Welcome Human!");
     }
   }
-  
+
   // get all data in form and return object
-  function getFormData() {
-    var form = document.getElementById("gform");
-    var elements = form.elements; // all form elements
+  function getFormData(form) {
+    var elements = form.elements;
+
     var fields = Object.keys(elements).filter(function(k) {
-          // the filtering logic is simple, only keep fields that are not the honeypot
           return (elements[k].name !== "honeypot");
     }).map(function(k) {
       if(elements[k].name !== undefined) {
@@ -30,75 +29,95 @@ function validEmail(email) { // see:
     }).filter(function(item, pos, self) {
       return self.indexOf(item) == pos && item;
     });
-    var data = {};
-    fields.forEach(function(k){
-      data[k] = elements[k].value;
-      var str = ""; // declare empty string outside of loop to allow
-                    // it to be appended to for each item in the loop
-      if(elements[k].type === "checkbox"){ // special case for Edge's html collection
-        str = str + elements[k].checked + ", "; // take the string and append 
-                                                // the current checked value to 
-                                                // the end of it, along with 
-                                                // a comma and a space
-        data[k] = str.slice(0, -2); // remove the last comma and space 
-                                    // from the  string to make the output 
-                                    // prettier in the spreadsheet
-      }else if(elements[k].length){
-        for(var i = 0; i < elements[k].length; i++){
-          if(elements[k].item(i).checked){
-            str = str + elements[k].item(i).value + ", "; // same as above
-            data[k] = str.slice(0, -2);
+
+    var formData = {};
+    fields.forEach(function(name){
+      var element = elements[name];
+      
+      // singular form elements just have one value
+      formData[name] = element.value;
+
+      // when our element has multiple items, get their values
+      if (element.length) {
+        var data = [];
+        for (var i = 0; i < element.length; i++) {
+          var item = element.item(i);
+          if (item.checked || item.selected) {
+            data.push(item.value);
           }
         }
+        formData[name] = data.join(', ');
       }
     });
-  
+
     // add form-specific values into the data
-    data.formDataNameOrder = JSON.stringify(fields);
-    data.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-    data.formGoogleSendEmail = form.dataset.email || ""; // no email by default
-  
-    console.log(data);
-    return data;
+    formData.formDataNameOrder = JSON.stringify(fields);
+    formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+    formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+
+    console.log(formData);
+    return formData;
   }
-  
-  function handleFormSubmit(event) {  // handles form submit withtout any jquery
+
+  function handleFormSubmit(event) {  // handles form submit without any jquery
     event.preventDefault();           // we are submitting via xhr below
-    var data = getFormData();         // get the values submitted in the form
-  
-    /* OPTION: Remove this comment to enable SPAM prevention, see README.md  */
+    var form = event.target;
+    var data = getFormData(form);         // get the values submitted in the form
+
+    /* OPTION: Remove this comment to enable SPAM prevention, see README.md */
     if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
       return false;
     }
-  
-    if( !validEmail(data.email) ) {   // if email is not valid show error
-      document.getElementById("email-invalid").style.display = "block";
-      return false;
+
+    if( data.email && !validEmail(data.email) ) {   // if email is not valid show error
+      var invalidEmail = form.querySelector(".email-invalid");
+      if (invalidEmail) {
+        invalidEmail.style.display = "block";
+        return false;
+      }
     } else {
-      var url = event.target.action;  //
+      disableAllButtons(form);
+      var url = form.action;
       var xhr = new XMLHttpRequest();
       xhr.open('POST', url);
       // xhr.withCredentials = true;
       xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
       xhr.onreadystatechange = function() {
-          console.log( xhr.status, xhr.statusText )
+          console.log(xhr.status, xhr.statusText);
           console.log(xhr.responseText);
-          document.getElementById("gform").style.display = "none"; // hide form
-          document.getElementById("thankyou_message").style.display = "block";
+          form.reset();
+          var formElements = form.querySelector(".form-elements")
+          if (formElements) {
+            formElements.style.display = "none"; // hide form
+          }
+          var thankYouMessage = form.querySelector(".thankyou_message");
+          if (thankYouMessage) {
+            thankYouMessage.style.display = "block";
+          }
           return;
       };
       // url encode form data for sending as post data
       var encoded = Object.keys(data).map(function(k) {
-          return encodeURIComponent(k) + "=" + encodeURIComponent(data[k])
-      }).join('&')
+          return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+      }).join('&');
       xhr.send(encoded);
     }
   }
+  
   function loaded() {
     console.log("Contact form submission handler loaded successfully.");
     // bind to the submit event of our form
-    var form = document.getElementById("gform");
-    form.addEventListener("submit", handleFormSubmit, false);
+    var forms = document.querySelectorAll("form.gform");
+    for (var i = 0; i < forms.length; i++) {
+      forms[i].addEventListener("submit", handleFormSubmit, false);
+    }
   };
   document.addEventListener("DOMContentLoaded", loaded, false);
-  
+
+  function disableAllButtons(form) {
+    var buttons = form.querySelectorAll("button");
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+  }
+})();
